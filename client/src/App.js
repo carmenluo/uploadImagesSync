32aa9b8f52ecc7ce11e50dfcc1ee0602688b0883
carmenluo <Sdluojm@gmail.com>
2019-11-20 13:54:34 -0500

    setup app

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+const Input =(props)=>(
+  <input type="file" name="file-input" multiple {...props} />
+)
+function App() {
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const onChange = (e) => {
+    console.log(e.target.files)
+  }
+  return <div className='container'>
+    <form className='form' onSubmit={onSubmit}>
+    <div>
+          <Input onChange={onChange} />
+          <button type="submit">Submit</button>
+        </div>
+    </form>
+  </div>
+}
+
+export default App;
